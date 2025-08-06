88572ac8ae6f001ec4bb6b1482eba45e4f529508
RubenCuevas79 <sr_ruben2000@yahoo.es>
2025-08-06 09:39:02 +0200

    segundo intento

---
+++ src/js/js_1.js
@@ -0,0 +1,24 @@
+const input = document.getElementById('text');
+const botAñadir= document.getElementById('agregar_tarea');
+const ul = document.getElementById('lisBa');
+const par = document.getElementById('noPendiente');
+const lisBa = document.getElementById('lisBa')
+let nu= [' ', ' ', ' ',  ' ' ];
+function apretar(botAñadir){
+
+ lisBa= document.createElement('<li id="uno"></li>')
+ function (){
+   if ( nu [0]  ){
+    
+
+
+
+
+   }
+
+if(lisBa== String)
+{
+    document.
+}
+
+ }
